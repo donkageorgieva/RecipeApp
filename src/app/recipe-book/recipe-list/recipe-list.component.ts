8a0b4ff12f32c9f1d5b77fc24ddafcd58f59104e
donkageorgieva <DoniGeorgieva16@gmail.com>
2022-03-31 11:17:31 +0300

    initial setup

---
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Pizza',
+      'Awesome Pizza',
+      'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    ),
+    new Recipe(
+      'Spaghetti',
+      'Awesome Spaghetti',
+      'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    ),
+  ];
+
+  ngOnInit(): void {}
+}
